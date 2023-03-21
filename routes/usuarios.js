3b0f55c16980b631be7db6bfe3bef6d2aa826072
Luis ]avier Martinez Fernandez <luisjavier004@hotmail.com>
2023-03-20 22:56:49 -0600

    finished proyect

---
+++ routes/usuarios.js
@@ -0,0 +1,58 @@
+import { Router } from 'express';
+import { check } from 'express-validator';
+import {
+  usuariosDelete,
+  usuariosGet,
+  usuariosPatch,
+  usuariosPost,
+  usuariosPut,
+} from '../controllers/usuarios.js';
+import {
+  emailExiste,
+  esRoleValido,
+  existeUsuarioPorId,
+} from '../helpers/db-validators.js';
+import { validarCampos } from '../middlewares/validar-campos.js';
+
+const router = Router();
+
+router.get('/', usuariosGet);
+
+router.put(
+  '/:id',
+  [
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom(existeUsuarioPorId),
+    check('rol').custom(esRoleValido),
+    validarCampos,
+  ],
+  usuariosPut,
+);
+
+router.post(
+  '/',
+  [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe ser de más de 6 letras').isLength({
+      min: 6,
+    }),
+    check('correo', 'El correo no es válido').isEmail().custom(emailExiste),
+    check('rol').not().isEmpty().custom(esRoleValido),
+    validarCampos,
+  ],
+  usuariosPost,
+);
+
+router.delete(
+  '/:id',
+  [
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom(existeUsuarioPorId),
+    validarCampos,
+  ],
+  usuariosDelete,
+);
+
+router.patch('/', usuariosPatch);
+
+export default router;
